3255887530ac069941714fbdd01c950d03fdb939
Andre Ananda Putra <andreanandaputra35@gmail.com>
2025-02-13 16:03:50 +0700

    Create options.lua

---
+++ nvim/lua/configs/options.lua
@@ -0,0 +1,17 @@
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+
+vim.opt.fillchars = "eob: "
+
+vim.cmd("colorscheme tokyonight")
+
+vim.cmd("highlight Normal ctermbg=none guibg=none")
+vim.cmd("highlight NormalNC ctermbg=none guibg=none")
+
+vim.cmd("highlight NeoTreeNormal ctermbg=none guibg=none")
+vim.cmd("highlight NeoTreeNormalNC ctermbg=none guibg=none")
