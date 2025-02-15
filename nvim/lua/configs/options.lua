cb5a40404955d50c2148d8a6978ebd542d69f037
Andre Ananda Putra <andreanandaputra35@gmail.com>
2025-02-15 21:19:31 +0700

    Update options.lua

--- nvim/lua/configs/options.lua
+++ nvim/lua/configs/options.lua
@@ -1,17 +1,9 @@
 vim.opt.number = true
-vim.opt.expandtab = true
-vim.opt.tabstop = 2
+vim.opt.tabstop = 1
 vim.opt.shiftwidth = 2
-vim.opt.smartindent = true
-vim.opt.termguicolors = true
-vim.opt.wrap = false
-
 vim.opt.fillchars = "eob: "
-
 vim.cmd("colorscheme tokyonight")
-
 vim.cmd("highlight Normal ctermbg=none guibg=none")
 vim.cmd("highlight NormalNC ctermbg=none guibg=none")
-
 vim.cmd("highlight NeoTreeNormal ctermbg=none guibg=none")
 vim.cmd("highlight NeoTreeNormalNC ctermbg=none guibg=none")
